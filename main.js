dabd232d9d48219bc0ab0b19f07e267e128b9dc2
pablobuenocursos <pablobuenocursos@gmail.com>
2024-12-01 11:26:01 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,5 @@
 console.log('Ejecutando el contenido de main.js');
 console.log("Variable de ambiente MI_VARIABLE: ", process.env.MI_VARIABLE);
+console.log("La variable centralizada es: ", process.env.VARIABLE_CENTRALIZADA);
+
+
